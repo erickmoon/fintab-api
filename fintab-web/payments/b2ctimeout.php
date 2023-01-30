e9987fe26b773a6c06325000f2c13624c6281c65
erickmoon <41348452+erickmoon@users.noreply.github.com>
2023-01-30 17:26:10 +0300

    Delete fintab-web directory

--- fintab-web/payments/b2ctimeout.php
+++
@@ -1,5 +0,0 @@
-<?php
-$postData = file_get_contents('php://input');
-$filePath = "output.txt";
-$file = fopen($filePath,"a");
-fwrite($file, $postData);
--- fintab-web/payments/secured_confirmation.php
+++
@@ -1,9 +0,0 @@
-<?php
-function writeFile($name, $string) {
-    $filename = $name.".txt"; 
-    $fp = fopen($filename,"a+");  
-    fputs($fp,$string); 
-    fclose($fp);  
-}
-
-?>
